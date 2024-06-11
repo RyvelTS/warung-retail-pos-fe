5b4b1e57da43f3b3cd168453dafd2fa795ff2a07
Ryvel Stamber <ryvel.timothy@gmail.com>
2024-06-12 00:00:15 +0700

    feat(home):update homepage design

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { AuthService } from '../../core/services/auth.service';
 import { Router } from '@angular/router';
 import { PrimaryLayoutComponent } from '../../shared/layouts/primary-layout/primary-layout.component';
+import { AngularMaterialModule } from '../../shared/modules/angular-material/angular-material.module';
+import { AngularFormsModule } from '../../shared/modules/angular-forms/angular-forms.module';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [PrimaryLayoutComponent],
+  imports: [ AngularMaterialModule, AngularFormsModule, PrimaryLayoutComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
