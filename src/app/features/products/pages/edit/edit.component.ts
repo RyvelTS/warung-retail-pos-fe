68ac52702d4be5e2b5616bd0a4174982ed42715e
MikhaelAdrielPratamaGana <mikhael.adriel.pratama.gana@gmail.com>
2024-06-18 23:52:41 +0700

    feat(Product):Add UI form Create Product and Update Product

--- src/app/features/products/pages/edit/edit.component.ts
+++ src/app/features/products/pages/edit/edit.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { AngularMaterialModule } from '../../../../shared/modules/angular-material/angular-material.module';
+import { PrimaryLayoutComponent } from '../../../../shared/layouts/primary-layout/primary-layout.component';
+import { ButtonComponent } from '../../../../shared/components/button/button.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-edit',
   standalone: true,
-  imports: [],
+  imports: [
+    AngularMaterialModule,
+    PrimaryLayoutComponent,
+    ButtonComponent,
+    RouterModule,
+  ],
   templateUrl: './edit.component.html',
-  styleUrl: './edit.component.scss'
+  styleUrl: './edit.component.scss',
 })
-export class EditComponent {
-
-}
+export class EditComponent {}
