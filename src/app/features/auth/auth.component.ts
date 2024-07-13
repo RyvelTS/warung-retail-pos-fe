af754d8b89b642347083c6e5e6b5003c1508ee1a
Ryvel Stamber <ryvel.timothy@gmail.com>
2024-07-13 12:23:17 +0700

    fix(auth):update auth pages using component layout route

---
+++ src/app/features/auth/auth.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-auth',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './auth.component.html',
+  styleUrl: './auth.component.scss'
+})
+export class AuthComponent {
+
+}
--- src/app/features/auth/auth.routes.ts
+++ src/app/features/auth/auth.routes.ts
@@ -2,16 +2,29 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { guestGuard } from '../../core/guards/guest.guard';
+import { AuthComponent } from './auth.component';
 
 export const authRoutes: Routes = [
     {
         path: 'login',
-        component: LoginComponent,
-        canMatch: [guestGuard]
+        component: AuthComponent,
+        canMatch: [guestGuard],
+        children: [
+            {
+                path: '',
+                loadComponent: () => import('./pages/login/login.component').then(m => m.LoginComponent),
+            },
+        ]
     },
     {
         path: 'register',
-        component: RegisterComponent,
-        canMatch: [guestGuard]
+        component: AuthComponent,
+        canMatch: [guestGuard],
+        children: [
+            {
+                path: '',
+                loadComponent: () => import('./pages/register/register.component').then(m => m.RegisterComponent),
+            },
+        ]
     }
 ];
